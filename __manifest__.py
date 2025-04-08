494cedabd42f88b11327fe5567c7132d991b3043
MyBusinessPlus-2 <ivan.correa@peope.online>
2025-04-08 10:41:25 -0500

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Form Action on Website',
+    'version': '1.0',
+    'summary': 'Trigger custom actions when a form is submitted',
+    'category': 'Website',
+    'depends': ['website', 'mail'],  # mail = if you want to send emails
+    'data': [
+        'views/contact_form_template.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
