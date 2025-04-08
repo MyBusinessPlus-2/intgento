fb5c22034d5b775badcdcee309937d55103feac9
MyBusinessPlus-2 <ivan.correa@peope.online>
2025-04-08 11:19:49 -0500

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
     'version': '1.0',
     'summary': 'Trigger custom actions when a form is submitted',
     'category': 'Website',
-    'depends': ['website', 'mail'],  # mail = if you want to send emails
+    'depends': ['website', 'mail', 'crm'],  # mail = if you want to send emails
     'data': [
         'views/contact_form_template.xml',
         'views/menu.xml',  # <== nuevo archivo
